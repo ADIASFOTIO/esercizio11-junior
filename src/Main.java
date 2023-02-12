a71dd7fb5a4e758ab49d3d51bd71102f0e1681d9
macbook <jnrfotio@gmail.com>
2023-02-12 21:44:09 +0100

    Esercizio1ADIASJUNIOR

--- src/Main.java
+++ src/Main.java
@@ -10,24 +10,19 @@ public class Main {
         ArrayList<Libro> libri = new ArrayList<>();
         ArrayList<Dvd> dvds = new ArrayList<>();
         ArrayList<RachettaDaPadel> rachettaDaPadels = new ArrayList<>();
-        ArrayList<String> cars = new ArrayList<String>();
-        Libro lib1 = new Libro("12x435", "STORIA", "Amore ", 0.5, 45, 200);
-        Libro lib2 = new Libro("ZX%&rg", "GUERRA", "SCIENZA ", 1, 50, 140);
         Dvd dv1 = new Dvd("hip hop", "love", 0.5, 5, 5 );
         Dvd dv2 = new Dvd("hip haap", "loverr", 0.5, 2, 7);
         RachettaDaPadel rch1 = new RachettaDaPadel("Rotonda", 1, 10);
         RachettaDaPadel rch2 = new RachettaDaPadel("AGioccia", 1.5, 9);
-        libri.add(lib1);
-        libri.add(lib2);
         dvds.add(dv1);
         dvds.add(dv2);
-        cars.add("jeux");
-        cars.add("macabo");
         rachettaDaPadels.add(rch1);
         rachettaDaPadels.add(rch2);
         Negozio negozio1 = new Negozio(libri, dvds, rachettaDaPadels);
         negozio1.addDvd("amore", "hiphop",0.5,5,3);
-        System.out.println(dvds.size());
+        negozio1.addLibri("lover","SCUOLA","Fantasy",500,3,3);
+        negozio1.addLibri("12xrr35", "Students", "needs ", 6000, 45, 200);
+        System.out.println(libri.size());
 
         /// ci permette di andare nell ArrayList per cercare
         for (Dvd c : dvds) {
